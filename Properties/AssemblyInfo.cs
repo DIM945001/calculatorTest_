55447398f275111553b828d1b8f35331088e0f32
DIM945001 <dmitrij1630626@gmail.com>
2021-10-11 11:05:36 +0400

    11

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("calculatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("calculatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1118af48-7d51-424e-ab07-264e04641a8d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace calculatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Summa10and10return20()
+        {
+           int a =  10;
+           int b =  10;
+           int expected =  20;
+
+            calculator_Class c = new calculator_Class();
+            int actual = c.Summa(a, b);
+
+            Assert.AreEqual(actual, expected);
+        }
+        
+        [TestMethod]
+        public void Minus10and10return0()
+        {
+            int a = 10;
+            int b = 10;
+            int expected = 0;
+
+            calculator_Class c = new calculator_Class();
+            int actual = c.minus(a, b);
+
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
